c326d3df26b47263f7cfbf973d2ada67fac1ba43
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2021-03-10 23:55:19 -0300

    inclusão de autorização

--- src/main/java/com/productapi/endpoint/ProductEndPoint.java
+++ src/main/java/com/productapi/endpoint/ProductEndPoint.java
@@ -1,8 +1,12 @@
 package com.productapi.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,9 +15,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.Console;
-import java.util.List;
 
 import com.productapi.domain.models.Product;
 import com.productapi.repository.ProductRepository;
@@ -64,6 +65,7 @@ public class ProductEndPoint {
     }
 
     @DeleteMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> delete(@PathVariable Long id) {
         try {
             var product = _productRepository.findById(id);
@@ -81,8 +83,9 @@ public class ProductEndPoint {
     }
 
     @GetMapping
-    public ResponseEntity<?> get() {
-        return new ResponseEntity<>(new Response(true, "", _productRepository.findAll()), HttpStatus.OK);
+    public ResponseEntity<?> get(Pageable pageable, @AuthenticationPrincipal UserDetails userDetails) {
+        System.out.println(userDetails);
+        return new ResponseEntity<>(new Response(true, "", _productRepository.findAll(pageable)), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
--- src/main/java/com/productapi/repository/ProductRepository.java
+++ src/main/java/com/productapi/repository/ProductRepository.java
@@ -1,11 +1,10 @@
 package com.productapi.repository;
 
 import com.productapi.domain.models.Product;
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface ProductRepository extends CrudRepository<Product, Long> {
+public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
     List<Product> findByNameIgnoreCaseContaining(String name);
 }
