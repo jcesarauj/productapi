fda117e85b0a08619f147212b8ac8517ff1a8625
Julio Cesar de Araujo <34680147+jcesarauj@users.noreply.github.com>
2021-03-22 22:19:12 -0300

    Inclusão tratamento de erros

---
+++ src/main/java/com/productapi/handler/RestResponseExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.productapi.handler;
+
+import java.io.IOException;
+
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+
+@ControllerAdvice
+public class RestResponseExceptionHandler extends DefaultResponseErrorHandler {
+    @Override
+    public boolean hasError(ClientHttpResponse response) throws IOException {
+        System.out.println("Inside Error");
+        return super.hasError(response);
+    }
+
+    @Override
+    public void handleError(ClientHttpResponse response) throws IOException {
+        System.out.println("Doing something with body");
+        super.handleError(response);
+    }
+}
